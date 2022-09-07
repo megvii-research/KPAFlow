f3f558bc937a70a659ef9d7482a582e28833d2a0
Ao Luo <47421121+LA30@users.noreply.github.com>
2022-09-07 17:16:52 +0800

    files updated

---
+++ eval_kitti.sh
@@ -0,0 +1,2 @@
+# C + T -> K
+python evaluate.py --model=weights/KPAFlow-CT2K.pth  --dataset=kitti --mixed_precision
---
+++ eval_sintel.sh
@@ -0,0 +1,2 @@
+# C + T -> S
+python evaluate.py --model=weights/KPAFlow-CT2S.pth  --dataset=sintel --mixed_precision
